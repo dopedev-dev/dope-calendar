36458cc6f8fbbb73cb8f863535c5cb0054b948be
amirsafaricg <a.safari1380@gmail.com>
2025-12-08 10:11:08 +0330

    fix vbite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     vue(), 
+    tailwindcss(),
     dts({
       insertTypesEntry: true,
       rollupTypes: true // Bundles types into a single file
