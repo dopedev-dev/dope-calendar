aa826ff54bec54a732df74441b5485eff82d3e08
amirsafaricg <a.safari1380@gmail.com>
2025-12-13 09:30:50 +0330

    bug fixes for style imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
 import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),
     dts({
       insertTypesEntry: true,
       rollupTypes: true,
